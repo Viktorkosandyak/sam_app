3ee83d8dbc3bdc614e3c44a9c29ba8ea96cc0bfe
Viktorkosandyak <kosandyak1996@gmail.com>
2018-10-19 20:21:06 +0300

    Finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  #  test "invalid signup information" do
+  #   get signup_path
+  #   assert_no_difference 'User.count' do
+  #     post users_path, user: { name: "",
+  #                              email: "user@invalid",
+  #                              password: "foo",
+  #                              password_confirmation: "bar" }
+  #   end
+  #   assert_template 'users/new'
+  # end
+
   test "should get new" do
     get :new
     assert_response :success
   end
-
+  
 end
